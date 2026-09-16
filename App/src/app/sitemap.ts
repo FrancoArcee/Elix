75bd5369ea5ed7eb006169752b192a2402d5bb54
FrancoArcee <franarce.2004@gmail.com>
2026-09-16 14:45:08 -0300

    Normaliza URLs

--- App/src/app/sitemap.ts
+++ App/src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://www.elixfragancias.com.ar";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://elixfragancias.com.ar";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPages: MetadataRoute.Sitemap = [
